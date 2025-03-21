4cdd30639517b3d58e5782401245e87c34014249
sai200598 <ram18raju@gmail.com>
2025-03-21 17:29:11 +0530

    file update

--- src/main/java/Base/Baseclass.java
+++ src/main/java/Base/Baseclass.java
@@ -1,5 +1,24 @@
 package Base;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Baseclass {
+	protected WebDriver driver;
+	
+	@BeforeClass
+	public void Browserlanuch() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.snapdeal.com/");
+		
+	}
 
 }
--- src/main/java/BasePages/Homepage.java
+++ src/main/java/BasePages/Homepage.java
@@ -1,5 +1,22 @@
 package BasePages;
 
-public class Homepage {
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+
+import Base.Baseclass;
+
+public class Homepage extends Baseclass {
+	 private By Searchpage = By.xpath("//input[@id='inputValEnter']");
+	 
+	 @BeforeMethod
+	 public void Entertext(String Text) {
+		WebElement Searchbox =  driver.findElement(Searchpage);
+		Searchbox.sendKeys(Text);
+	 }
+	
 
 }
